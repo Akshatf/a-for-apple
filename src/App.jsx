0a937e18f96b69b04dd88dd4c7513fd01b30c62c
Akshatf <akshatfarkya7@gmail.com>
2025-05-22 00:46:49 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import { Analytics } from "@vercel/analytics/next"
 import { allParagraphs, images, videos, sr, gola, all, instagramReels } from './data';
 
 const App = () => {
@@ -421,6 +422,7 @@ const App = () => {
           )}
         </>
       )}
+      <Analytics />
     </div>
   );
 };
